93ce6c265e837a07ac027c16f4d1c6009b14b80d
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2022-01-14 19:27:08 +0100

    Creational patterns - prototype - part2

--- src/main/java/edu/sda/sample/patterns/creational/prototype/PrototypeDemo.java
+++ src/main/java/edu/sda/sample/patterns/creational/prototype/PrototypeDemo.java
@@ -1,7 +1,22 @@
 package edu.sda.sample.patterns.creational.prototype;
 
+import java.util.List;
+
 public class PrototypeDemo {
     public static void main(String[] args) {
+        //template
+        ShoppingList shoppingList = new ShoppingList();
 
+        try {
+            ShoppingList todays = (ShoppingList) shoppingList.clone();
+            List<String> todaysShoppings = todays.getShoppings();
+            todaysShoppings.add("piwo");
+            todaysShoppings.add("kiełbaski");
+            todays = new ShoppingList(todaysShoppings);
+
+            System.out.println("Break point");
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
     }
 }
